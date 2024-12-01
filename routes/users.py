72cfae63c34c343a11aeb4ed50de07a2a11f163d
DESKTOP-78RFPES\surface <ebrahimzademohammadhossein@gmail.com>
2024-12-01 21:19:03 +0330

    edit users.py

--- routes/users.py
+++ routes/users.py
@@ -7,14 +7,12 @@ from crud import get_all_users, get_user_by_id
 
 router = APIRouter()
 
-# دریافت لیست کاربران (فقط توسط ادمین)
 @router.get("/")
 def get_users(session: Session = Depends(get_session), user = Depends(get_current_user)):
     check_role(user, ["Admin"])
     users = get_all_users(session)
     return users
 
-# دریافت جزئیات یک کاربر خاص (فقط توسط ادمین)
 @router.get("/{user_id}/")
 def get_user(user_id: int, session: Session = Depends(get_session), user = Depends(get_current_user)):
     check_role(user, ["Admin"])
@@ -23,7 +21,3 @@ def get_user(user_id: int, session: Session = Depends(get_session), user = Depen
         raise HTTPException(status_code=404, detail="User not found")
     return user_data
 
-# دریافت پروفایل کاربر جاری
-@router.get("/me/")
-def get_my_profile(user = Depends(get_current_user)):
-    return {"id": user.id, "username": user.username, "email": user.email, "role": user.role}
