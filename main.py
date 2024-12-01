91d6f26ba8c7c38a8c1f78080ffef19071ceeccd
DESKTOP-78RFPES\surface <ebrahimzademohammadhossein@gmail.com>
2024-12-01 13:37:02 +0330

    add routes in main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
-
+from database import create_db_and_tables
+from models import User, Post, Comment
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from crud import create_post, get_posts, get_post_by_id, update_post, delete_post, create_comment, get_comments_for_post
+from database import get_session
+from typing import Optional
+from routes import posts, comments, users
+from auth import router as auth_router
 app = FastAPI()
 
 
@@ -7,9 +15,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+    print("create successfully!")
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(posts.router, prefix="/posts", tags=["Posts"])
+app.include_router(comments.router, prefix="/comments", tags=["Comments"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
 
