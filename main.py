d8ba8d0419682105bda9471454c1e789112dbfb8
DESKTOP-78RFPES\surface <ebrahimzademohammadhossein@gmail.com>
2024-12-03 21:55:08 +0330

    clean code main.py
    Took 31 seconds

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from database import get_session
 from typing import Optional
 from routes import posts, comments, users
 from auth import router as auth_router
+
 app = FastAPI()
 
 
@@ -14,13 +15,14 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
     print("create successfully!")
 
+
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(posts.router, prefix="/posts", tags=["Posts"])
 app.include_router(comments.router, prefix="/comments", tags=["Comments"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
-
